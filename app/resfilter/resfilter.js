4e7cd8dadd12e32fe88f5d4dcde045b357ffcf70
xueing.bo <xueting.bo@pictureworks.biz>
2016-12-05 22:02:24 +0800

    getPhotosByConditions

--- app/resfilter/resfilter.js
+++ app/resfilter/resfilter.js
@@ -1,8 +1,10 @@
 var park=require("./park");
 var resInfo=require("./resInfo");
 var user=require("./user");
+var photo = require('./photo');
 module.exports={
     park:park,
     resInfo:resInfo,
-    user:user
+    user:user,
+    photo: photo
 }
