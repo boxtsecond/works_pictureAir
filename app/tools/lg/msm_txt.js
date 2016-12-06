59a9be80b83e0fe2f96dc5575f2fc52ccd0a781d
meteor <meteor.liu@pictureworks.biz>
2016-12-06 16:10:46 +0800

    no message

--- app/tools/lg/msm_txt.js
+++ app/tools/lg/msm_txt.js
@@ -2,6 +2,6 @@
  * Created by meteor on 16/11/25.
  */
 module.exports= [
-    {code:"en-US",type:"validateCode",sign:"[pictureAir] ",content:" Hello, Your verification code is:%validateCode%."},
+    {code:"en-US",type:"validateCode",sign:"[pictureAir]",content:" Hello, Your verification code is:%validateCode%."},
     {code:"zh-CN",type:"validateCode",sign:"【相集】",content:"  您好，您的手机验证码为：%validateCode%"}
 ];
