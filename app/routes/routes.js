49dd0cda2291a774c11b177609e7923723479f6d
xueing.bo <xueting.bo@pictureworks.biz>
2016-12-08 13:04:16 +0800

    no message

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -9,7 +9,6 @@ module.exports=function(app){
     app.use('/auth',require('./auth'));
     //Guest
     app.use('/g',auth.authGuest,require('./guestRoute'));
-    // app.use('/g',require('./guestRoute'));
     app.use('/p',auth.authUser,require('./privateRoute'));
     app.use('*',auth.authGuest);
     return app;
