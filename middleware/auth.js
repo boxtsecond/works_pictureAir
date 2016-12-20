91aac8fd7e8a30ec87ac636683e1f9b9bdd1ea67
meteor <meteor.liu@pictureworks.biz>
2016-12-20 15:55:26 +0800

    no message

--- middleware/auth.js
+++ middleware/auth.js
@@ -28,10 +28,11 @@ function getAccessToken(req){
 function authGuest(req,res,next){
     var token=getAccessToken(req);
     if(token) {
-       access_token.verifyGuestAccess_token(token.toString().trim()).then(function(toke){
+       return access_token.verifyGuestAccess_token(token.toString().trim()).then(function(toke){
           req.ext.params.token=toke;
           req.ext.params.token.expire_in=Math.floor(toke.exp-Math.floor(Date.now() / 1000));
           next();
+         return obj;
       }).catch(function(err){
           return res.ext.json({ status: 421, msg: 'unauthorized',result:{}});
       })
@@ -42,7 +43,7 @@ function authGuest(req,res,next){
 function authUser(req,res,next){
     var token=getAccessToken(req);
     if(token) {
-          access_token.verifyAccess_token(token).then(function(toke){
+        return  access_token.verifyAccess_token(token).then(function(toke){
             // 从redis中获取
             return  redisclient.get("access_token:"+toke.audience).then(function(access_token){
                 if(access_token){
@@ -62,6 +63,7 @@ function authUser(req,res,next){
             req.ext.params.userId=obj.userid;
             req.ext.params.token.expire_in=Math.floor(obj.toke.exp-Math.floor(Date.now() / 1000));
               next();
+             return obj;
         }).catch(function(err){
             return res.ext.json({ status: 421, msg: 'unauthorized'});
         })
